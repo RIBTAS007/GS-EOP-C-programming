9b51e0de6215c3b39b2d4d90087aaaac18efff77
SATBIR SINGH <RIBTAS007@OUTLOOK.COM>
2020-04-30 14:45:29 +0530

    day 1 codes added

---
+++ DAY 1/d1c2.c
@@ -0,0 +1,13 @@
+//Using scanf to take inputs
+
+#include<stdio.h>
+
+int main()             
+{
+	 int a,b;
+     scanf("%d%d",&a,&b);
+	 printf("\n"); 	 
+	printf("a=%d\tb=%d\n",a,b);
+	return 0;
+	
+}
